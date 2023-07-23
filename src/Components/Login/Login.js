f48b43f82c8608d91bff6379aab5e7ea543df3c5
CodeGeno <kmatagne@gmail.com>
2023-07-23 04:22:21 +0200

    Updated Login Component/alerts

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -16,8 +16,13 @@ function Login() {
   const { loggedIn, setLoggedIn } = useContext(LoginContext)
   const { userDetail, setUserDetail } = useContext(UserContext)
   const [login, setLogin] = useState([
-    { email: 'kmatagne@gmail.com', pwd: 'kaelkael1' },
+    { email: 'JohnDoe@gmail.com', pwd: 'john1234' },
   ])
+  const [alert, setAlert] = useState({
+    show: false,
+    alertType: 'success',
+    message: '',
+  })
   const [showCreateAccount, setShowCreateAccount] = useState(false)
   const navigate = useNavigate()
   useEffect(() => {
@@ -32,107 +37,121 @@ function Login() {
       .then(() => {
         console.log('disconnected')
       })
-      .catch(() => {
-        console.log('erreur')
+      .catch((error) => {
+        console.log(error)
       })
   }
-  const accountLogIn = (e) => {
+  const accountLogIn = async (e) => {
     e.preventDefault()
     const email = login[0].email
     const password = login[0].pwd
-    signInWithEmailAndPassword(auth, email, password).then((cred) => {
-      console.log('Loged in:', cred.user)
-      setUserDetail([{ email: cred.user.email }])
-      setLoggedIn(true)
-    })
-    console.log(userDetail)
+    try {
+      await signInWithEmailAndPassword(auth, email, password).then((cred) => {
+        console.log('Loged in:', cred.user)
+        setUserDetail([{ email: cred.user.email }])
+        setLoggedIn(true)
+      })
+    } catch (error) {
+      displayAlert('Wrong credentials', 'danger')
+    }
   }
 
   //AddUserToDatabase
   const documentRef = collection(db, 'Users')
 
   //Create Account + Adding User collection to db
-  const accountCreation = (e) => {
+  const accountCreation = async (e) => {
     e.preventDefault()
     const email = login[0].email
     const password = login[0].pwd
-    createUserWithEmailAndPassword(auth, email, password).then((cred) => {
-      console.log('user created:', cred.user.auth.currentUser.email)
-      setUserDetail([{ email: cred.user.auth.currentUser.email }])
-      setLogin([{ email: '', pwd: '' }])
-      const credentials = cred
-      const addUserToDb = async () => {
-        await addDoc(documentRef, {
-          email: credentials.user.auth.currentUser.email,
-        })
-      }
-      addUserToDb()
-    })
+    try {
+      await createUserWithEmailAndPassword(auth, email, password).then(
+        (cred) => {
+          setUserDetail([{ email: cred.user.auth.currentUser.email }])
+          setLogin([{ email: '', pwd: '' }])
+          const credentials = cred
+          const addUserToDb = async () => {
+            await addDoc(documentRef, {
+              email: credentials.user.auth.currentUser.email,
+            })
+          }
+          addUserToDb()
+        }
+      )
+      displayAlert('Account created', 'success')
+    } catch (error) {
+      if (error.message === 'Firebase: Error (auth/email-already-in-use).')
+        displayAlert('This email is already in use', 'danger')
+    }
   }
-
+  const displayAlert = (message, alertType) => {
+    setAlert({ show: true, message: message, alertType: alertType })
+    setTimeout(() => {
+      setAlert({ show: false, message: '' })
+    }, 3000)
+  }
   const handleValueChange = (e) => {
     let temp = login[0]
     temp[e.target.name] = e.target.value
-    console.log(temp)
     let res = []
     res[0] = temp
     setLogin(res)
   }
-  useEffect(() => {
-    console.log(login)
-  }, [login])
 
   return (
     <>
-      {!showCreateAccount && (
-        <>
-          <Wrapper>
-            <form
-              className='form'
-              onSubmit={(e) => {
-                console.log('hello')
-              }}
-            >
-              <div>Login to your account:</div>
-
-              <FormRow
-                type='text'
-                value={login.email}
-                handleChange={handleValueChange}
-                labelText='E-mail'
-                name='email'
-              />
-              <FormRow
-                type='password'
-                value={login.pwd}
-                handleChange={handleValueChange}
-                labelText='Password'
-                name='pwd'
-              />
-              <div className='login-btn-container'>
-                <button
-                  className='btn'
-                  type='submit'
-                  onClick={(e) => {
-                    accountLogIn(e)
-                  }}
-                >
-                  Log In
-                </button>
-                <button
-                  className='btn'
-                  type='button'
-                  onClick={(e) => {
-                    setShowCreateAccount(!showCreateAccount)
-                  }}
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-          </Wrapper>
-        </>
-      )}
+      <>
+        <Wrapper>
+          <form className='form'>
+            <div>
+              {showCreateAccount
+                ? 'Create your account'
+                : 'Login to your account:'}
+            </div>
+            {alert.show && alert.alertType === 'danger' ? (
+              <>
+                <div className='danger'>{alert.message}</div>
+              </>
+            ) : (
+              <div className='success'>{alert.message}</div>
+            )}
+            <FormRow
+              type='text'
+              value={login[0].email}
+              handleChange={handleValueChange}
+              labelText='E-mail'
+              name='email'
+            />
+            <FormRow
+              type='password'
+              value={login[0].pwd}
+              handleChange={handleValueChange}
+              labelText='Password'
+              name='pwd'
+            />
+            <div className='login-btn-container'>
+              <button
+                className='btn'
+                type='submit'
+                onClick={(e) => {
+                  showCreateAccount ? accountCreation(e) : accountLogIn(e)
+                }}
+              >
+                {showCreateAccount ? 'Create account' : 'Login'}
+              </button>
+              <button
+                className='btn'
+                type='button'
+                onClick={(e) => {
+                  setShowCreateAccount(!showCreateAccount)
+                }}
+              >
+                {showCreateAccount ? 'Cancel' : 'Create account'}
+              </button>
+            </div>
+          </form>
+        </Wrapper>
+      </>
     </>
   )
 }
